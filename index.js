4a3335b35844d475430cfeb87676bdf45ce4b328
unknown <mdfaruk01720875872>
2025-06-14 17:50:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
+const cors = require('cors');
+const app = express();
+const cookieParser = require('cookie-parser');
+const port = process.env.PORT || 3000
+
+
+
+
+// med el wer
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+
+app.get('/', (req, res) =>{
+    res.send('hello world')
+})
+console.log()
+
+
+// mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hpujglf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+    try{
+            await client.connect();
+            const coursesCollection = client.db('courseDB').collection('courses')
+
+            // get
+            app.get('/courses', async(req, res) =>{
+                const cursor = coursesCollection.find().sort({createdAt: -1}).limit(6);
+                const result = await cursor.toArray();
+                res.send(result);
+            })
+
+            // post
+            app.post('/courses', async(req, res) =>{
+                const newCourse = req.body;
+                const result = await coursesCollection.insertOne(newCourse);
+                res.send(result)
+            })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+              await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () =>{
+    console.log(`listen port : ${port}`)
+})
